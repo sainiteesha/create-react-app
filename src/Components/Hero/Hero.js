349d1d6d87453bfb6067abeb3475012bcb5e04c1
Jadala Sai niteesha <69296529+sainiteesha@users.noreply.github.com>
2021-08-29 19:15:09 +0530

    Add files via upload

---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../Hero/Hero.css'
+
+
+export default function Hero() {
+    return (        
+    <section className="home">
+    <div className="content">
+    <h3>Welcome To
+    Lets Grow More</h3>
+    </div>
+    
+    </section>
+    )
+}
